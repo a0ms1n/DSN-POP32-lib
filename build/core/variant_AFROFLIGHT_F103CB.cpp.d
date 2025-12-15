2972a398382a48b8c62324d5219efc11b6713379
a0ms1n <150027949+a0ms1n@users.noreply.github.com>
2025-12-15 22:30:42 +0700

    Delete build directory

--- build/core/variant_AFROFLIGHT_F103CB.cpp.d
+++
@@ -1,2 +0,0 @@
-/home/a0ms1n/Hidden\ Desktop/Robots/DSN-POP32-lib/build/core/variant_AFROFLIGHT_F103CB.cpp.o: \
- /home/a0ms1n/.arduino15/packages/INEX_STM32/hardware/stm32/2.4.0/variants/STM32F1xx/F103C8T_F103CB(T-U)/variant_AFROFLIGHT_F103CB.cpp
--- build/libraries/SrcWrapper/LL/stm32yyxx_ll_hrtim.c.d
+++
@@ -1,2 +0,0 @@
-/home/a0ms1n/Hidden\ Desktop/Robots/DSN-POP32-lib/build/libraries/SrcWrapper/LL/stm32yyxx_ll_hrtim.c.o: \
- /home/a0ms1n/.arduino15/packages/INEX_STM32/hardware/stm32/2.4.0/libraries/SrcWrapper/src/LL/stm32yyxx_ll_hrtim.c
