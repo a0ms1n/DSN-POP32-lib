8da301945703c5f6999357901b2ca29ab6885a7f
sevenjiji <seven.vayupak@gmail.com>
2026-01-16 14:45:14 +0700

    Aom_kak

--- SLPA-72/Mid-Korn/Mid-Korn.ino
+++ SLPA-72/Mid-Korn/Mid-Korn.ino
@@ -12,7 +12,7 @@ int Fast = 200;
 void Run1();
 void Run2();
 void Run3();
-void Run4();
+void MusicJaa();
 
 void setup(){
     BasicMenu.buttons.push_back({
@@ -29,7 +29,7 @@ void setup(){
     BasicMenu.buttons[0].callback = Run1; // Run 1
     BasicMenu.buttons[1].callback = Run2; // Run 2
     BasicMenu.buttons[2].callback = Run3; // Run 3
-    BasicMenu.buttons.push_back({"Run 4",Run4});
+    BasicMenu.buttons.push_back({"Music",MusicJaa});
     Front.__Track = 700;
     Back.__Track = 800;
     motors.setSpeedRange(90,250);
@@ -41,7 +41,7 @@ void loop(){
     BasicMenu.show();
 }
 
-void Run4(){
+void MusicJaa(){
     Sound::RickRoll();
 }
 
